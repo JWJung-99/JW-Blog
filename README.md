ebb86cd5e88b130ae64dcae228f0691b8665c24b
JWJung-99 <39busy@naver.com>
2025-08-12 19:06:28 +0900

    Reset: 프로젝트 재설정

--- README.md
+++
@@ -1,7 +0,0 @@
-# JW-Blog
-
-<br />
-
-## 👋 Introduction
-
-Front-end 블로그를 개발 중입니다.
--- src/app/posts/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function PostsPage() {
-  return (
-    <main>
-      <h1 className="text-3xl font-bold">게시글 페이지입니다.</h1>
-    </main>
-  );
-}
