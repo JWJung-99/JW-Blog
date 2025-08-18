18b0b661c211faf50cef7440ba0a7710131706b3
JWJung-99 <39busy@naver.com>
2025-08-18 20:32:33 +0900

    🎉 Init: 프로젝트 초기 설정

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends(
+    'next',
+    'next/core-web-vitals',
+    'next/typescript',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ),
+  ...compat.plugins('react', '@typescript-eslint'),
+  ...compat.config({
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+    },
+  }),
+];
+
+export default eslintConfig;
